d8928e15cd8fd88560ea6d400700f2a4ca007a93
aswin benny <84976659+aswinbenny@users.noreply.github.com>
2025-08-09 22:44:15 +0530

    updates to eda and added wrangle function in utils

---
+++ utils/wrangle.py
@@ -0,0 +1,42 @@
+import pandas as pd
+
+def assign_season(date):
+    month = date.month
+    if month in range(6, 10):
+        return 'Southwest Monsoon'
+    elif month in range(10, 12):
+        return 'Post Monsoon'
+    elif month in range(3, 6):
+        return 'Summer'
+    else:
+        return 'Winter'
+
+def wrangle(df):
+    df['Arrival_Date'] = pd.to_datetime(df['Arrival_Date'], format='%d/%m/%Y')
+    df[['Max_Price', 'Modal_Price']] = df[['Max_Price', 'Modal_Price']].astype(float)
+
+    df['Is_VFPCK'] = df['Market'].str.contains('VFPCK', case=False)
+    df['Season'] = df['Arrival_Date'].apply(assign_season)
+
+    df = df.groupby(
+        ['Commodity', 'Variety', 'Grade', 'Arrival_Date', 'Market', 'Season', 'Is_VFPCK'],
+        as_index=False
+        ).agg({
+        'Modal_Price': 'mean',
+        'Max_Price': 'mean',
+        'Min_Price': 'mean'
+        })
+    
+    df['Product_Type'] = df['Commodity'] + '|' + df['Variety'] + '|' + df['Grade']
+
+    product_type_counts = df['Product_Type'].value_counts()
+    valid_product_types = product_type_counts[product_type_counts > 15].index
+    df = df[df['Product_Type'].isin(valid_product_types)]
+
+    df.drop(columns=['Commodity', 'Variety', 'Grade'], inplace=True)     
+
+    column_order = ['Product_Type', 'Arrival_Date', 'Market', 'Is_VFPCK', 'Season', 'Modal_Price', 'Max_Price', 'Min_Price']
+    df = df[column_order]
+
+    df = df.sort_values(by=['Product_Type', 'Arrival_Date', 'Market']).reset_index(drop=True)
+    return df
